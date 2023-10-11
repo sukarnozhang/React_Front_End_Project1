33fdaa1521ec808356f65ce562034ec08c26b1cd
hanisahmk <khalilatulhanisah@gmail.com>
2023-10-12 00:16:40 +0800

    added magnifiying glass and changed CSS

--- src/components/SearchItem.js
+++ src/components/SearchItem.js
@@ -3,7 +3,7 @@ import styles from "../allCssStyling/SearchItem.modules.css";
 import { MagnifyingGlass } from "phosphor-react";
 import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
-import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
+import ButtonToolbar from "react-bootstrap/ButtonToolbar";
 
 function SearchItem({ value, label, onChange }) {
   // const handleChange = (event) => {       //PartA: declare this handler if not using inline version
@@ -17,19 +17,24 @@ function SearchItem({ value, label, onChange }) {
 
       {/* Inline version of passing a onChange handler to  */}
 
-      <ButtonToolbar >
-      <InputGroup >
-        <InputGroup.Text id="btnGroupAddon"> @ </InputGroup.Text>
-        <Form.Control
-          type="text"
-          aria-describedby="btnGroupAddon"
-          value={value}
-          label={label}
-          placeholder="search items..."
-          onChange={(e) => onChange(e.target.value)}
-          
-        />
-      </InputGroup>
+      <ButtonToolbar>
+        <InputGroup>
+          <InputGroup.Text id="btnGroupAddon">
+            <MagnifyingGlass
+              size={15}
+              className="search-icon"
+              id="responsive-icon"
+            />
+          </InputGroup.Text>
+          <Form.Control
+            type="text"
+            aria-describedby="btnGroupAddon"
+            value={value}
+            label={label}
+            placeholder="search items..."
+            onChange={(e) => onChange(e.target.value)}
+          />
+        </InputGroup>
       </ButtonToolbar>
       {/* <img src={searchIcon} alt="Search" className="search-icon" /> */}
       {/* <MagnifyingGlass size={32} className="search-icon" id="responsive-icon"/> */}
